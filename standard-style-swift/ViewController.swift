29238a6cbfc0a58cd94d92926915090e7b6f2ea5
Raven <rdev1163@outlook.com>
2018-04-02 11:16:56 -0700

    Initial Commit

---
+++ standard-style-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  standard-style-swift
+//
+//  Created by Raven on 4/2/18.
+//  Copyright © 2018 RDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
